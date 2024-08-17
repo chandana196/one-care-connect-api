62c27ffa99740a0f2577c35eecf0eaa1b269881f
chandana <chandanabehera96@gmail.com>
2024-08-17 08:02:51 +0530

    adding allowed origin

--- src/main/java/com/one/care/config/WebConfig.java
+++ src/main/java/com/one/care/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.one.care.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${ALLOWED_ORIGINS}")
+    private String allowedOrigins;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        String[] origins = allowedOrigins.split(",");
         registry.addMapping("/**") // Allow all paths
-                .allowedOrigins("http://localhost:3000") // Allow requests from your React frontend
+                .allowedOrigins(origins) // Allow requests from your React frontend
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow specified HTTP methods
                 .allowedHeaders("*") // Allow all headers
                 .allowCredentials(true); // Allow credentials (e.g., cookies)
